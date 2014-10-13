9f82b58822a08c596e16814aedebed12352271c1
WilsomLi <lijia@lijiadeMacBook-Pro.local>
2014-10-14 01:46:56 +0800

    Add run demo
    Add run demo

---
+++ client/AgunRun/Assets/Scripts/AniSprite.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AniSprite : MonoBehaviour {
+
+	public static int DIR_LEFT = 0;
+	public static int DIR_RIGHT = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PlayMotion(int frameCnt,int direction) {
+		int index = (int)(Time.time * frameCnt);
+		index = index % frameCnt;
+
+		int dirVal = -1;
+		if(direction == AniSprite.DIR_LEFT) {
+			dirVal = 1;
+		}
+
+		float scaleX = dirVal * 1.0f / frameCnt;
+		Vector2 size = new Vector2(scaleX,1);
+		Vector2 offset = new Vector2(dirVal * index*scaleX,0);
+//		Debug.Log (scaleX);
+		renderer.material.mainTextureScale = size;
+		renderer.material.mainTextureOffset = offset;
+	}
+}
---
+++ client/AgunRun/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 10;
+
+	public Material runMaterial;
+	public Material jumpMaterial;
+
+	private AniSprite aniSprite;
+	private CharacterController controller;
+
+	private Vector3 vec;
+
+	// Use this for initialization
+	void Start () {	
+		aniSprite = GetComponent ("AniSprite") as AniSprite;
+		controller = GetComponent ("CharacterController") as CharacterController;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		vec = new Vector3(Input.GetAxis("Horizontal"),0,0);
+		transform.renderer.material = runMaterial;
+		int dir = Input.GetAxis ("Horizontal") < 0 ? AniSprite.DIR_LEFT : AniSprite.DIR_RIGHT;
+		aniSprite.PlayMotion (10, dir);
+		vec *= speed;
+		controller.Move (vec* Time.deltaTime);
+	}
+}
